9880af431000fb336ebf8ca3ca3902c7b90c1d8e
Surya Prakash <suryaprakash@Suryas-MacBook-Air.local>
2026-06-07 20:29:33 +0530

    Add routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("./routes/githubRoutes"));
+app.use("/api", require("./routes/githubroutes"));
 
 app.get("/", (req, res) => {
   res.send("GitHub Analyzer API Running");
